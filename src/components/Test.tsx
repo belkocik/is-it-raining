43275417810a86db8be6809f4609376a76167ced
bubuq3 <62507500+bubuq3@users.noreply.github.com>
2023-04-19 17:26:53 +0200

    Create configs and first component

--- src/components/Test.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {};
-
-export const Test = (props: Props) => {
-	return (
-		<div>
-			<h1>Test</h1>
-		</div>
-	);
-};
---
+++ src/hooks/useTranslations.ts
@@ -0,0 +1,8 @@
+import { pl_PL } from '../dictionaries';
+
+type Props = {};
+
+export const useTranslations = (props: Props) => {
+  // TODO: Create logic to switch languages
+  return pl_PL;
+};
